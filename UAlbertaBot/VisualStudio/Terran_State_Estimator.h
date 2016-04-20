8c8b3143d739425942175abf559b79c824a77f97
Gregory Ledray <g.ledray@gmail.com>
2016-04-20 11:38:52 -0500

    moved terran_state_estimator.h class variables
    They were in the UAlbertaBot namespace instead of inside both the namespace and the class itself

--- UAlbertaBot/VisualStudio/Terran_State_Estimator.h
+++ UAlbertaBot/VisualStudio/Terran_State_Estimator.h
@@ -5,31 +5,31 @@
 
 namespace UAlbertaBot
 {
-	int worker_count = 0;
-	double estimated_enemy_worker_count; //calculated via enemyWorkerCalculation();
-	
-	double military_resource_estimation;
-	double military_resource_estimation_error;//calculated via enemyMilitaryCalculation();
-	
-	double observed_building_resources;
-	double estimated_enemy_building_resources;//calculated via enemyBuildingCalcuation();
-	
-	double observed_upgrade_resources;
-	double estimated_upgrade_resources;//calculated via enemyUpgradeResourcesCalcuation();
-	
-	double estimated_enemy_minerals_on_hand;
-	double estimated_enemy_gas_on_hand;
-	double estimated_total_enemy_minerals_mined;
-	double estimated_total_enemy_gas_mined;//calculated via enemyResourceCalcuation();
-	
-	double observed_enemy_supply;
-	double estimated_enemy_supply;//calculated via enemySupplyCalcuation();
-
 	class Terran_State_Estimator
 	{
 		BWAPI::Race					    _selfRace;
 		BWAPI::Race					    _enemyRace;
-
+		
+		int worker_count = 0;
+		double estimated_enemy_worker_count; //calculated via enemyWorkerCalculation();
+		
+		double military_resource_estimation;
+		double military_resource_estimation_error;//calculated via enemyMilitaryCalculation();
+		
+		double observed_building_resources;
+		double estimated_enemy_building_resources;//calculated via enemyBuildingCalcuation();
+		
+		double observed_upgrade_resources;
+		double estimated_upgrade_resources;//calculated via enemyUpgradeResourcesCalcuation();
+		
+		double estimated_enemy_minerals_on_hand;
+		double estimated_enemy_gas_on_hand;
+		double estimated_total_enemy_minerals_mined;
+		double estimated_total_enemy_gas_mined;//calculated via enemyResourceCalcuation();
+		
+		double observed_enemy_supply;
+		double estimated_enemy_supply;//calculated via enemySupplyCalcuation();
+	
 	public:
 		bool enemyWorkerCalculation();
 		bool enemyMilitaryCalculation();
